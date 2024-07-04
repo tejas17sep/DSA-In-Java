335a7fad1c98a144f56c5f4ee1477d4d6a9720aa
tejas17sep <tejasbachhao17@gmail.com>
2024-07-05 02:27:55 +0530

    First Commit

---
+++ Day 7/src/Peak_Index_In_mountain_array.java
@@ -0,0 +1,6 @@
+public class Peak_Index_In_mountain_array {
+    public static void main(String[] args) {
+        //https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+        
+    }
+}
---
+++ Day 7/src/search_in_rotated_sorted_array.java
@@ -0,0 +1,61 @@
+public class search_in_rotated_sorted_array {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int ans = pivot( nums);
+        System.out.println(ans);
+    }
+
+    static int search(int[] nums,int target,int s,int l){
+        int x= pivot( nums);
+        // if we didi not grt the pivot means the array was not rotated
+        // so just do normal binary search
+        if (x==-1){
+            // just do normal binary search
+            return binarySearch(nums,target,0,nums.length);
+        }
+        if (nums[x]==target){
+            return x;
+        }
+        if (target>=nums[0]){
+
+        }
+
+    }
+    static int binarySearch(int[] nums, int target, int s, int l){
+        while(s<l){
+            int mid = s + (l-s)/2;
+            if (target<nums[mid]){
+                l=mid-1;
+            } else if (target>nums[mid]) {
+                s=mid+1;
+            }
+            else {
+                return mid;
+            }
+
+        }
+    }
+
+            static int pivot(int[] nums){
+            int s = 0,l = nums.length-1;
+
+            while(s<=l){
+            int mid = s + (l-s)/2;
+            if(mid<l && nums[mid]>nums[mid +1])
+            {
+                return l=mid;
+            }
+            if(mid > s && nums[mid]<nums[mid-1]){
+                return s=mid-1;
+            }
+            if(nums[s]>=nums[mid]){
+                l=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+
+        }
+            return -1;
+    }
+}
