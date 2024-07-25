c6f5cdb465d4369d40c11b914b12b342e20cc4ea
tejas17sep <tejasbachhao17@gmail.com>
2024-07-26 00:33:40 +0530

    First Commit

---
+++ Bubble_Sort/src/Cyclic_Sort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Cyclic_Sort {
+    public static void main(String[] args) {
+        int [] arr = {3,5,2,1,4};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cycle(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if (arr[i] != arr[correct]){
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+    }
+}
